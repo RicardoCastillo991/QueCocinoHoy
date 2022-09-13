6a832d04d671fa1258978137634d394ce53ae019
RicardoCastillo991 <r.castillo39@alumnos.santotomas.cl>
2022-09-12 23:22:30 -0300

    Version 2 con el inicio de la app

--- app/src/main/java/com/example/quecocinohoy/MainActivity.java
+++ app/src/main/java/com/example/quecocinohoy/MainActivity.java
@@ -7,14 +7,28 @@ import android.view.*;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnRegistrar;
+    private Button btnIngresar;
+    private EditText txtCorreoIngresar;
+    private EditText txtPasswordIngresar;
+    private TextView txtError;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtCorreoIngresar = (EditText) findViewById(R.id.txtCorreo);
+        txtPasswordIngresar = (EditText) findViewById(R.id.txtPassword);
+        txtError = (TextView) findViewById(R.id.txtError);
+
+        btnIngresar = (Button)findViewById(R.id.btnIngresar);
         btnRegistrar = (Button)findViewById(R.id.btnRegistrarse);
 
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
@@ -24,8 +38,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intento);
             }
         });
-    }
 
+        btnIngresar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String correoIngresar;
+                String passwordIngresar;
+                correoIngresar = txtCorreoIngresar.getText().toString();
+                passwordIngresar = txtPasswordIngresar.getText().toString();
+                if(correoIngresar.equals("hola1@gmail.com") && passwordIngresar.equals("1234")){
+                    Intent intento = new Intent(MainActivity.this, MainActivityInicio.class);
+                    startActivity(intento);
+                }
+                else
+                {
+                    txtCorreoIngresar.setText("");
+                    txtPasswordIngresar.setText("");
+                    txtError.setVisibility(View.VISIBLE);
+                }
+        };
+    });
+    }
 
 
 }
