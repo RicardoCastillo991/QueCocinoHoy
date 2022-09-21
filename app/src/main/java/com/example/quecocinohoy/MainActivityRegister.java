ab94870c41e8ecb9c7dbc517ec6949e9f5226d86
RicardoCastillo991 <r.castill39@alumnos.santotomas.cl>
2022-09-21 13:11:08 -0300

    spinner funcionando y imagen del splash

--- app/src/main/java/com/example/quecocinohoy/MainActivityRegister.java
+++ app/src/main/java/com/example/quecocinohoy/MainActivityRegister.java
@@ -3,12 +3,64 @@ package com.example.quecocinohoy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivityRegister extends AppCompatActivity {
+public class MainActivityRegister extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private TextView tvEdad;
+    private SeekBar seekBarEdad;
+    String [] sexos = {"Masculino", "Femenino", "No binario", "Otros"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_register);
+        //se va a declarar la parte logica con lo visual.
+        Spinner spin = (Spinner) findViewById(R.id.spinnerSexo);
+        SeekBar seekBarEdad = (SeekBar)findViewById(R.id.seekBar);
+        spin.setOnItemSelectedListener(this);
+
+
+        //se llama al adapter para instanciar la lista
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,sexos);
+
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spin.setAdapter(aa);
+
+        //utilizacion del seekbar para saber la edad del usuario.
+
+        seekBarEdad.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tvEdad.setText("Edad : " + i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
