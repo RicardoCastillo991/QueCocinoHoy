3ab1ca1655e8879af05dfa4b9b46435bb40e3baf
RicardoCastillo991 <r.castill39@alumnos.santotomas.cl>
2022-09-29 09:46:34 -0300

    apliacion con registro de contraseña y validar correo

--- app/src/main/java/com/example/quecocinohoy/MainActivityRegister2Step.java
+++ app/src/main/java/com/example/quecocinohoy/MainActivityRegister2Step.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,8 +20,13 @@ public class MainActivityRegister2Step extends AppCompatActivity {
         Button btnInicio = (Button) findViewById(R.id.btnInicio);
         EditText txtPass = (EditText) findViewById(R.id.txtPassRegister);
         EditText txtPassDos = (EditText) findViewById(R.id.txtPassDosRegister);
+        CheckBox ckBoxMostrarPass = (CheckBox)findViewById(R.id.ckboxMostrarPass);
 
+        if(ckBoxMostrarPass.isChecked() == true){
+            txtPass.setInputType(InputType.TYPE_CLASS_TEXT);
+            txtPassDos.setInputType(InputType.TYPE_CLASS_TEXT);
 
+        }
         btnInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
